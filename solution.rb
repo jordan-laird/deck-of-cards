8d1c582f0f8b4897d2322dd44e7e8fe1293dc1c4
jordan-laird <jordanmack.laird@gmail.com>
2018-10-22 12:18:59 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,46 @@
+require 'pry'
+
 class Deck
+
+    attr_accessor :cards
+
+    def initialize()
+        @cards = []
+        generate_cards
+    end
+
+    def generate_cards
+        ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"].each do |rank|
+            ["Heart", "Diamond","Spade", "Club"].each do |suit|
+               @cards <<  Card.new(suit, rank)
+            end
+        end
+
+        @cards
+    end
+
+    def choose_card
+        chosen_card = @cards.delete_at(rand(@cards.length))
+
+        # binding.pry
+
+        return chosen_card
+
+    end
+    
 
 end
 
 class Card
 
+    attr_accessor :rank, :suit
+
+    def initialize(suit,rank)
+        @rank = rank
+        @suit = suit
+    end
+
+
 end
+
+# binding.pry
